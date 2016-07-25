cb752d726c44c60229b612e65c4c0c5754d448cb
césar <csar@porttil-cesar>
2016-07-26 00:08:21 +0200

    Add files via upload

---
+++ build/Debug/Cygwin-Windows/_ext/aa7cd5e9/conversionbilletes.o.d
@@ -0,0 +1,2 @@
+build/Debug/Cygwin-Windows/_ext/aa7cd5e9/conversionbilletes.o: \
+ ../conversionbilletes/conversionbilletes.c
---
+++ build/Debug/Cygwin-Windows/_ext/aa7cd5e9/leerImporte.o.d
@@ -0,0 +1,2 @@
+build/Debug/Cygwin-Windows/_ext/aa7cd5e9/leerImporte.o: \
+ ../conversionbilletes/leerImporte.c
